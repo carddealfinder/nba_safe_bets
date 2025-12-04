e38068ceba4aa462641e023a0d10259b574a3aee
Your Name <srmichael86@gmail.com>
2025-12-04 11:07:42 -0500

    Fix logging system with simple print logger for Streamlit Cloud

--- safe_bets_app/nba_safe_bets/utils/logging_config.py
+++ safe_bets_app/nba_safe_bets/utils/logging_config.py
@@ -1,18 +1,3 @@
-import logging
-
-# Create single global logger
-log = logging.getLogger("nba_safe_bets")
-log.setLevel(logging.INFO)
-
-# Console handler
-ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)
-
-# Formatter
-formatter = logging.Formatter("[%(asctime)s] [%(levelname)s] %(message)s",
-                              datefmt="%Y-%m-%d %H:%M:%S")
-ch.setFormatter(formatter)
-
-# Attach handler
-if not log.handlers:
-    log.addHandler(ch)
+def log(message: str):
+    """Simple print logger for Streamlit Cloud compatibility."""
+    print(message)
